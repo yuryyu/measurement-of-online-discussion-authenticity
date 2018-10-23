431e859bd6cb3139f6f2033e6a1b8067798b331c
layercake1 <leahkaye6@gmail.com>
2018-10-23 13:12:14 +0300

    Update bad_actors/preprocessing_tools/leadspotting_posts_importer.py

--- bad_actors/preprocessing_tools/leadspotting_posts_importer.py
+++ bad_actors/preprocessing_tools/leadspotting_posts_importer.py
@@ -13,14 +13,14 @@ class LeadspottingPostsImporter(CsvImporter):
     def create_post_dict_from_row(self, row):
         #guid = unicode(generate_random_guid())
         post_dict = {}
-        post_dict["content"] = unicode(row["title"])
-        post_dict["date"] = unicode(row["DATE"])
-        post_dict["guid"] = unicode(row["tweetId"])
-        post_dict["author"] = unicode(row["name"])
-        author_guid = unicode(compute_author_guid_by_author_name(row["name"]).replace('-', ''))
+        post_dict["content"] = row["title"]
+        post_dict["date"] = row["DATE"]
+        post_dict["guid"] = row["tweetId"]
+        post_dict["author"] = row["name"]
+        author_guid = compute_author_guid_by_author_name(row["name"]).replace('-', '')
         post_dict["author_guid"] = author_guid
         post_dict["references"] = u""
         post_dict["domain"] = self._domain
-        post_dict["author_osn_id"] = unicode(row["userId"])
-        post_dict["url"] = unicode("https://twitter.com/{0}/status/{1}".format(post_dict["author"], row["userId"]))
+        post_dict["author_osn_id"] = row["userId"]
+        post_dict["url"] = "https://twitter.com/{0}/status/{1}".format(post_dict["author"], row["userId"])
         return post_dict
