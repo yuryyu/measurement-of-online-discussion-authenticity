5a6a487d7aa94dbee0f3e556745b0cf72ad8355e
layercake1 <leahkaye6@gmail.com>
2018-10-24 19:43:59 +0300

    t

--- bad_actors/preprocessing_tools/leadspotting_posts_importer.py
+++ bad_actors/preprocessing_tools/leadspotting_posts_importer.py
@@ -17,7 +17,7 @@ class LeadspottingPostsImporter(CsvImporter):
         post_dict["date"] = unicode(row["DATE"])
        # post_dict["guid"] = guid.replace('-', '')
         post_dict["guid"] = unicode(row["tweetId"])
-        post_dict["author"] = row["name"].replace(' ', '_').decode('utf-8')
+        post_dict["author"] = row["name"].decode('utf-8')
         author_guid = compute_author_guid_by_author_name(unicode(row["name"]).replace('-', ''))
         post_dict["author_guid"] = author_guid
       #  post_dict["author_guid"] = unicode(row["userId"])
