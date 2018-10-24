5d16e3099ab3ed1deabc56f2ea736642522de898
layercake1 <leahkaye6@gmail.com>
2018-10-24 17:06:57 +0300

    Update bad_actors/count_rows.py

--- bad_actors/count_rows.py
+++ bad_actors/count_rows.py
@@ -2,5 +2,14 @@ file_path = "logs.txt"
 
 with open(file_path, 'r') as f:
     text = f.read()
-    count = text.count('(')
-    print count
+    text = text.replace('(', '')
+    string_list = text.split('),')
+    auth_id_list = []
+    duplicates = []
+    for string in string_list:
+        auth_id = string.split(',')[0]
+        if auth_id in auth_id_list:
+            duplicates.append(auth_id)
+        else:
+            auth_id_list.append(auth_id)
+    print duplicates
