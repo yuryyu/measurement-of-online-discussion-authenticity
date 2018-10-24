59e7a9b4655e86417f64df617c72901d37d039e4
layercake1 <leahkaye6@gmail.com>
2018-10-24 14:34:48 +0300

    Update bad_actors/preprocessing_tools/leadspotting_posts_importer.py

--- bad_actors/preprocessing_tools/leadspotting_posts_importer.py
+++ bad_actors/preprocessing_tools/leadspotting_posts_importer.py
@@ -18,9 +18,9 @@ class LeadspottingPostsImporter(CsvImporter):
        # post_dict["guid"] = guid.replace('-', '')
         post_dict["guid"] = unicode(row["tweetId"])
         post_dict["author"] = row["name"].replace(' ', '_').decode('utf-8')
-      #  author_guid = compute_author_guid_by_author_name(unicode(row["name"]).replace('-', ''))
-      #  post_dict["author_guid"] = author_guid
-        post_dict["author_guid"] = unicode(row["userId"])
+        author_guid = compute_author_guid_by_author_name(unicode(row["name"]).replace('-', ''))
+        post_dict["author_guid"] = author_guid
+      #  post_dict["author_guid"] = unicode(row["userId"])
         post_dict["references"] = u""
         post_dict["domain"] = self._domain
         post_dict["author_osn_id"] = unicode(row["userId"])
