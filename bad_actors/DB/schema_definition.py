bce0dd60cba832fb04bf9bd604aa2143df2d7b84
yuryyu <yuzbaster@gmail.com>
2018-10-30 16:06:31 +0000

    updated schema_definition

--- bad_actors/DB/schema_definition.py
+++ bad_actors/DB/schema_definition.py
@@ -167,8 +167,8 @@ class Campaigns(Base):
 
 class CampaignsData(Base):    
     __tablename__ = 'campaigns_data'
-    campaign_id         = Column(Integer,  unique=True, primary_key=True) 
-    tweet_id            = Column(Unicode, default=None)      
+    campaign_id         = Column(Integer, default=0) 
+    tweet_id            = Column(Unicode, unique=True, primary_key=True)      
     parent_tweet_id     = Column(Unicode, default=None) 
     url                 = Column(Unicode, default=None) 
     author_id           = Column(Unicode, default=None)
