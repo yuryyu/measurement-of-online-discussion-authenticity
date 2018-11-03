552fcd88d0f29084f676fe970ec4c07d24bf7e96
Yury Yurchenko <yuzbaster@gmail.com>
2018-11-03 13:55:05 +0000

    production run config

--- bad_actors/prediction.bat
+++ bad_actors/prediction.bat
@@ -1,3 +1,3 @@
 echo on
 cd C:\Users\Developer_1\Documents\GitHub\measurement-of-online-discussion-authenticity\bad_actors
-python run.py configuration\config_leadspotting_init_DB.ini
+python prediction.py configuration\config_prediction.ini
