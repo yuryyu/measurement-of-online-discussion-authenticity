1f6583fc5ff576bdcab2bea78e578107416055c2
layercake1 <leahkaye6@gmail.com>
2019-01-15 13:15:09 +0200

    n

--- bad_actors/data_exporter/unittests/test_ranked_authors_exporter.py
+++ bad_actors/data_exporter/unittests/test_ranked_authors_exporter.py
@@ -11,8 +11,10 @@ class TestRankedAuthorsExporter(TestCase):
         self._db = DB()
         self._db.setUp()
         self._ranked_authors_exporter = RankedAuthorsExporter(self._db)
+        self._author_guid_dict = {}
         self.make_authors_posts_and_connections()
         self.csv_location = self.config.eval('RankedAuthorsExporter', 'output_file_path')
+        self._threshold = self.config.eval('RankedAuthorsExporter', 'threshold')
 
     def tearDown(self):
         self._db.session.close_all()
@@ -26,116 +28,61 @@ class TestRankedAuthorsExporter(TestCase):
         with open(self.csv_location, 'rb') as f:
             reader = csv.reader(f)
             rows = list(reader)
-            print rows
+            rows.pop(0)
+            self.assertEqual(rows[0][4], '0.0')
+            speed_list = [a[4] for a in rows]
+            a = 0
+            i = 0
+            before_threshold = []
+            for sp in speed_list:
+                if sp > a:
+                    a = sp
+                    before_threshold.append(sp)
+                    i += 1
+                else:
+                    after_threshold = speed_list[i:len(speed_list)]
+                    break
+            assert before_threshold == sorted(before_threshold)
+            assert after_threshold == sorted(after_threshold)
+            claim_ids_before_threshold = [row[2] for row in rows[:i-1]]
+            claims_count_dict = dict((x, claim_ids_before_threshold.count(x)) for x in set(claim_ids_before_threshold))
+            for v in claims_count_dict.values():
+                assert v <= self._threshold
 
     def make_authors_posts_and_connections(self):
-        self._author_guid1 = compute_author_guid_by_author_name(u'BillGates')
-        author = Author()
-        author.name = u'BillGates'
-        author.domain = u'Microblog'
-        author.protected = 0
-        author.author_guid = self._author_guid1
-        author.author_screen_name = u'BillGates'
-        author.author_full_name = u'Bill Gates'
-        author.statuses_count = 10
-        author.author_osn_id = 149159975
-        author.followers_count = 12
-        author.created_at = datetime.datetime.strptime('2016-04-02 00:00:00', '%Y-%m-%d %H:%M:%S')
-        author.missing_data_complementor_insertion_date = datetime.datetime.now()
-        author.xml_importer_insertion_date = datetime.datetime.now()
-        self._db.add_author(author)
+        for j in range(30):
+            self._author_guid_dict['author_guid_' + str(j)] = compute_author_guid_by_author_name(unicode(j**2))
+            author = Author()
+            author.name = unicode(j**2)
+            author.domain = u'Microblog'
+            author.protected = 0
+            author.author_guid = self._author_guid_dict['author_guid_' + str(j)]
+            author.author_screen_name = unicode(j**2)
+            author.author_full_name = unicode(j**2)
+            author.statuses_count = 10
+            author.author_osn_id = 149159975
+            author.followers_count = 12
+            author.created_at = datetime.datetime.strptime('2016-04-02 00:00:00', '%Y-%m-%d %H:%M:%S')
+            author.missing_data_complementor_insertion_date = datetime.datetime.now()
+            author.xml_importer_insertion_date = datetime.datetime.now()
+            self._db.add_author(author)
 
-        for i in range(10):
-            post = Post()
-            post.post_id = u'TestPost' + str(i)
-            post.author = u'BillGates'
-            post.guid = u'TestPost' + str(i)
-            post.url = u'TestPost' + str(i)
-            tempDate = u'2016-05-05 00:00:00'
-            day = datetime.timedelta(1)
-            post.date = datetime.datetime.strptime(tempDate, '%Y-%m-%d %H:%M:%S') + day * i
-            post.domain = u'Microblog'
-            post.author_guid = self._author_guid1
-            post.content = u"InternetTV love it #wow"
-            post.xml_importer_insertion_date = datetime.datetime.now()
-            self._db.addPost(post)
-            claim_tweet_conn = Claim_Tweet_Connection()
-            claim_tweet_conn.claim_id = 'test_claim' + str(i)
-            claim_tweet_conn.post_id = post.post_id
-            self._db.session.merge(claim_tweet_conn)
-            self._db.commit()
-
-        self._author_guid2 = compute_author_guid_by_author_name(u'ZachServideo')
-        author = Author()
-        author.name = u'ZachServideo'
-        author.domain = u'Microblog'
-        author.protected = 0
-        author.author_guid = self._author_guid2
-        author.author_screen_name = u'ZachServideo'
-        author.author_full_name = u'Zach Servideo'
-        author.statuses_count = 110
-        author.author_osn_id = 40291482
-        author.created_at = datetime.datetime.strptime('2016-04-05 00:00:00', '%Y-%m-%d %H:%M:%S')
-        author.missing_data_complementor_insertion_date = datetime.datetime.now()
-        author.xml_importer_insertion_date = datetime.datetime.now()
-        author.friends_count = 12
-        self._db.add_author(author)
-
-        for i in range(10, 120):
-            post = Post()
-            post.post_id = u'TestPost' + str(i)
-            post.author = u'ZachServideo'
-            post.guid = u'TestPost' + str(i)
-            post.url = u'TestPost' + str(i)
-            tempDate = u'2016-05-05 00:00:00'
-            day = datetime.timedelta(1)
-            post.date = datetime.datetime.strptime(tempDate, '%Y-%m-%d %H:%M:%S') + day * i
-            post.domain = u'Microblog'
-            post.author_guid = self._author_guid2
-            post.content = u'OnlineTV love it http://google.com'
-            post.xml_importer_insertion_date = datetime.datetime.now()
-            self._db.addPost(post)
-            claim_tweet_conn = Claim_Tweet_Connection()
-            claim_tweet_conn.claim_id = 'test_claim' + str(i)
-            claim_tweet_conn.post_id = post.post_id
-            self._db.session.merge(claim_tweet_conn)
-            self._db.commit()
-
-        self._author_guid3 = compute_author_guid_by_author_name(u'AyexBee')
-        author = Author()
-        self._author_3_name = u'AyexBee'
-        author.name = self._author_3_name
-        author.domain = u'Microblog'
-        author.protected = 0
-        author.author_guid = self._author_guid3
-        author.author_screen_name = u'AyexBee'
-        author.author_full_name = u'AxB'
-        author.statuses_count = 100
-        author.author_osn_id = 100271909
-        author.created_at = datetime.datetime.strptime('2016-04-05 00:00:00', '%Y-%m-%d %H:%M:%S')
-        author.missing_data_complementor_insertion_date = datetime.datetime.now()
-        author.xml_importer_insertion_date = datetime.datetime.now()
-        author.followers_count = 12
-        self._db.add_author(author)
-
-        for i in range(120, 220):
-            post = Post()
-            post.post_id = u'TestPost' + str(i)
-            post.author = u'AyexBee'
-            post.guid = u'TestPost' + str(i)
-            post.url = u'TestPost' + str(i)
-            tempDate = u'2016-05-05 00:00:00'
-            day = datetime.timedelta(1)
-            post.date = datetime.datetime.strptime(tempDate, '%Y-%m-%d %H:%M:%S') + day * i
-            post.domain = u'Microblog'
-            post.author_guid = self._author_guid3
-            post.content = u'Security love it http://google.com'
-            post.xml_importer_insertion_date = datetime.datetime.now()
-            self._db.addPost(post)
-            claim_tweet_conn = Claim_Tweet_Connection()
-            claim_tweet_conn.claim_id = 'test_claim' + str(i)
-            claim_tweet_conn.post_id = post.post_id
-            self._db.session.merge(claim_tweet_conn)
-            self._db.commit()
-
-        self._db.commit()
+            for i in range(10):
+                post = Post()
+                post.post_id = u'TestPost' + unicode(j) + u'_' + unicode(i)
+                post.author = author.name
+                post.guid = u'TestPost' + unicode(j) + u'_' + unicode(i)
+                post.url = u'TestURL' + unicode(j) + u'_' + unicode(i)
+                tempDate = u'2016-05-05 00:00:00'
+                day = datetime.timedelta(1)
+                post.date = datetime.datetime.strptime(tempDate, '%Y-%m-%d %H:%M:%S') + day * i
+                post.domain = u'Microblog'
+                post.author_guid = self._author_guid_dict['author_guid_' + str(j)]
+                post.content = u"InternetTV love it #wow"
+                post.xml_importer_insertion_date = datetime.datetime.now()
+                self._db.addPost(post)
+                claim_tweet_conn = Claim_Tweet_Connection()
+                claim_tweet_conn.claim_id = u'test_claim_' + unicode(i)
+                claim_tweet_conn.post_id = post.post_id
+                self._db.session.merge(claim_tweet_conn)
+                self._db.commit()
