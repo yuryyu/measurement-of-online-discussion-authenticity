497d3b8b74317ab47a39f0e9535adfe7a24e7a75
Yury Yurchenko <yuzbaster@gmail.com>
2019-01-16 13:42:29 +0000

    Update flask3_run.bat

--- bad_actors/batch_files/flask3_run.bat
+++ bad_actors/batch_files/flask3_run.bat
@@ -1,4 +1,4 @@
 echo on
-cd C:\Users\Administrator\Documents\GitHub\measurement-of-online-discussion-authenticity\bad_actors
-rem cd C:\Users\Developer_1\Documents\GitHub\measurement-of-online-discussion-authenticity\bad_actors
+rem cd C:\Users\Administrator\Documents\GitHub\measurement-of-online-discussion-authenticity\bad_actors
+cd C:\Users\Developer_1\Documents\GitHub\measurement-of-online-discussion-authenticity\bad_actors
 python flask_api\flask_api3.py configuration\config_api.ini
